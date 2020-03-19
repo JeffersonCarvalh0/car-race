86a4f9ae8174d6b36706584d14b9dabbce20c87b
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-19 15:36:57 -0300

    chore: cleanup files from template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-const App = (): JSX.Element => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const Container = styled.div`
+  background-color: gray;
+`;
+
+const App = (): JSX.Element => <Container>Hello, World!</Container>;
 
 export default App;
