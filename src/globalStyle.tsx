60944f6b55e30ef81deeaca62fe7a13ef699830a
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-19 15:59:09 -0300

    feat(global-style): add global styles

---
+++ src/globalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    background-color: #e5e5e5;
+    font-family: Roboto;
+    font-size: 12px;
+  }
+`;
+
+export default GlobalStyle;
