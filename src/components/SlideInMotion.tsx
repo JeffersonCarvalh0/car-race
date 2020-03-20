7883e24849c293e43d006d70bf8be6ad9e5bb5e7
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-20 10:58:04 -0300

    feat(slide-in-motion): extract it to a separate component

---
+++ src/components/SlideInMotion.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Motion, spring } from 'react-motion';
+
+interface StyledProps {
+  x: number;
+  opacity: number;
+}
+const SlideInStyle = styled.section`
+  transform: ${(props: StyledProps) => `translateX(${props.x}px)`};
+  opacity: ${(props: StyledProps) => props.opacity};
+`;
+
+interface Props {
+  children: React.ReactNode;
+}
+const SlideInMotion = ({ children }: Props) => {
+  return (
+    <Motion
+      defaultStyle={{ x: -300, opacity: 0 }}
+      style={{ x: spring(0), opacity: spring(1) }}
+    >
+      {({ x, opacity }) => (
+        <SlideInStyle x={x} opacity={opacity}>
+          {children}
+        </SlideInStyle>
+      )}
+    </Motion>
+  );
+};
+
+export default SlideInMotion;
--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Motion, spring } from 'react-motion';
 
-interface StyledProps {
-  x: number;
-  opacity: number;
-}
+import SlideInMotion from './SlideInMotion';
 
 const Title = styled.h1`
-  flex: 1;
-  transform: ${(props: StyledProps) => `translateX(${props.x}px)`};
-  opacity: ${(props: StyledProps) => props.opacity};
   text-align: center;
   color: rgb(59, 134, 255);
 `;
@@ -19,18 +12,11 @@ interface Props {
   text: string;
 }
 
-const TitleHeader = (props: Props) => {
+const TitleHeader = ({ text }: Props) => {
   return (
-    <Motion
-      defaultStyle={{ x: -300, opacity: 0 }}
-      style={{ x: spring(0), opacity: spring(1) }}
-    >
-      {values => (
-        <Title x={values.x} opacity={values.opacity}>
-          {props.text}
-        </Title>
-      )}
-    </Motion>
+    <SlideInMotion>
+      <Title>{text}</Title>
+    </SlideInMotion>
   );
 };
 
