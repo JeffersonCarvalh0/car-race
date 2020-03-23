46aed00fcc6819eeb1779b6ebd8d80972d1a6066
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-23 14:57:20 -0300

    feat(centered-text): extract to a separate component

---
+++ src/components/CenteredText.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const CenteredText = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0 0 0 -25px;
+  color: red;
+`;
+
+export default CenteredText;
