2e824e7cde387da453074e2d91ce183b3dced9ae
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-23 12:49:17 -0300

    Updates

---
+++ precache-manifest.9ad40b1ba8ac25bd154990802cc42890.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6051b48627ad64768f376a1c3dd2da9f",
+    "url": "/car-race/index.html"
+  },
+  {
+    "revision": "a08f5c596365db3e8aed",
+    "url": "/car-race/static/js/2.f291279f.chunk.js"
+  },
+  {
+    "revision": "337af8776e89b99720ae0bcbd286cb0d",
+    "url": "/car-race/static/js/2.f291279f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92df0f24c52250711c15",
+    "url": "/car-race/static/js/main.0d3216c7.chunk.js"
+  },
+  {
+    "revision": "a50cb7ce8de6fe541f95",
+    "url": "/car-race/static/js/runtime-main.7e13bc1e.js"
+  },
+  {
+    "revision": "0b863896bf30b933f268e8d7d34ddc05",
+    "url": "/car-race/static/media/RetroGaming.0b863896.woff2"
+  },
+  {
+    "revision": "447cad1781b5a4b21df88240ec48980a",
+    "url": "/car-race/static/media/RetroGaming.447cad17.woff"
+  },
+  {
+    "revision": "4c19fc875e7ba1e6831129de3ab5ac0b",
+    "url": "/car-race/static/media/RetroGaming.4c19fc87.ttf"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/car-race/static/media/background.d0d2cd17.gif"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/car-race/static/media/car.56be0a4e.png"
+  }
+]);
