bacabc987cb266a67a11d32673312b1cfc79985b
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-24 13:20:39 -0300

    chore(assets): add obstacle and preload it

--- src/helpers/preloadAssets.ts
+++ src/helpers/preloadAssets.ts
@@ -1,23 +1,25 @@
 import car from '../assets/car.png';
+import obstacle from '../assets/obstacle.png';
 import background from '../assets/background.gif';
 import pausedBackground from '../assets/background-paused.png';
 
 const loadImage = (src: string) => {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.onload = () => resolve();
+    img.onload = resolve;
     img.onerror = reject;
     img.src = src;
   });
 };
 
-const preloadAssets = async (callback: Function) => {
+const preloadAssets = async (onFinishLoad: Function) => {
   await Promise.all([
     loadImage(car),
+    loadImage(obstacle),
     loadImage(background),
     loadImage(pausedBackground),
   ]);
-  callback();
+  onFinishLoad();
 };
 
 export default preloadAssets;
