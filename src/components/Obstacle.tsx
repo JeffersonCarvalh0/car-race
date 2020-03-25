25b41c8806be86d4173ecd19d3fc0683c8f76008
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-25 11:00:20 -0300

    feat(obstacle): replaced transitions with keyframes and refactored the code

--- src/components/Obstacle.tsx
+++ src/components/Obstacle.tsx
@@ -1,81 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import { Position } from '../components/Car';
 import obstacle from '../assets/obstacle.png';
 
 interface StyledProps {
-  y: number;
-  x: number;
-  scale: number;
+  initialX: number;
+  finalX: number;
+  isPaused: boolean;
   time: number;
 }
+
+const obstacleMovement = (props: StyledProps) => keyframes`
+  100% {
+    top: 80%;
+    transform: scale(1, 1);
+    left: ${`${props.finalX}%`};
+  }
+`;
+
 const StyledObstacle = styled.img`
   content: url(${obstacle});
   position: relative;
   margin-left: 5%;
   width: 20%;
   height: 20%;
-  top: ${(props: StyledProps) => `${props.y}%`};
-  left: ${(props: StyledProps) => `${props.x}%`};
-  transform: scale(${(props: StyledProps) => `${props.scale}, ${props.scale}`});
-  transition: ${(props: StyledProps) =>
-    `top ${props.time}ms linear, left ${props.time}ms linear, transform ${props.time}ms linear`};
+  top: 40%;
+  transform: scale(0.15, 0.15);
+  left: ${(props: StyledProps) => props.initialX}%;
+  animation: ${obstacleMovement} ${(props: StyledProps) => props.time}ms linear;
+  animation-play-state: ${(props: StyledProps) =>
+    props.isPaused ? 'paused' : 'running'};
 `;
-
-const getInitialX = (position: Position) => {
-  switch (position) {
-    case Position.Left:
-      return 30;
-    case Position.Middle:
-      return 33;
-    case Position.Right:
-      return 35;
-  }
-};
-
-const getFinalX = (position: Position) => {
-  switch (position) {
-    case Position.Left:
-      return 0;
-    case Position.Middle:
-      return 33.3;
-    case Position.Right:
-      return 66.6;
-  }
-};
-
-const initialValues = {
-  y: 40,
-  scale: 0.15,
-};
-
-const finalValues = {
-  y: 80,
-  scale: 1,
-};
 
 interface Props {
   position: Position;
   time: number;
+  isPaused: boolean;
 }
-const Obstacle = ({ position, time }: Props) => {
-  /* based on the velocity, I can calculate the position of the obstacle in any
-   * given time. Therefore, I just need to know how much time should be elapsed
-   * since the object was created and the current player's position to
-   * if there is a collision or not.*/
+const Obstacle = ({ position, time, isPaused }: Props) => {
+  const initialX = (() => {
+    switch (position) {
+      case Position.Left:
+        return 30;
+      case Position.Middle:
+        return 33;
+      case Position.Right:
+        return 35;
+    }
+  })();
 
-  const [y, setY] = useState(initialValues.y);
-  const [x, setX] = useState(getInitialX(position));
-  const [scale, setScale] = useState(initialValues.scale);
+  const finalX = (() => {
+    switch (position) {
+      case Position.Left:
+        return 0;
+      case Position.Middle:
+        return 33.3;
+      case Position.Right:
+        return 66.6;
+    }
+  })();
 
-  useEffect(() => {
-    setY(finalValues.y);
-    setX(getFinalX(position));
-    setScale(finalValues.scale);
-  }, [position]);
-
-  return <StyledObstacle y={y} x={x} scale={scale} time={time} />;
+  return (
+    <StyledObstacle
+      initialX={initialX}
+      finalX={finalX}
+      time={time}
+      isPaused={isPaused}
+    />
+  );
 };
 
 export default Obstacle;
