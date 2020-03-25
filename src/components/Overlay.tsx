11df5bad11657752e8037665be5950e9e29b7565
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-03-25 11:45:48 -0300

    feat(overlay): insert children below the text

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -5,10 +5,21 @@ import CenteredText from '../components/CenteredText';
 
 const Background = styled.div`
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: inherit;
   height: inherit;
   background-color: rgba(0, 0, 0, 0.4);
   z-index: 2;
+`;
+
+const StaticCenteredText = styled(CenteredText)`
+  position: static;
+  top: 0;
+  left: 0;
+  transform: translate(0, 0);
 `;
 
 interface Props {
@@ -18,7 +29,7 @@ interface Props {
 const Overlay = ({ text, children }: Props) => {
   return (
     <Background>
-      <CenteredText>{text}</CenteredText>
+      <StaticCenteredText>{text}</StaticCenteredText>
       {children}
     </Background>
   );
